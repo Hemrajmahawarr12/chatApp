01c391fbf1b96e1c0877d91629471337d828af91
Hemrajmahawarr12 <hemr843263@gmail.com>
2024-05-09 10:25:35 +0530

    add chatSection

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,28 @@
-import { Grid } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import React from 'react'
 
 function Chat() {
   return (
-    <Grid sx={{flex:"2"}}>Chat</Grid>
+    <Grid sx={{ flex: "2", borderLeft: "1px solid #dddddd35", borderRight: "1px solid #dddddd35" }}>
+      <Box>
+        <Box>
+          <Box sx={{padding:"20px",display:"flex",justifyContent:"space-between",borderBottom:"1px solid #dddddd59",alignItems:"center"}}>
+            <Box sx={{display:"flex",gap:"20px"}}>
+              <img style={{height:"60px",width:"60px", borderRadius:"50%",objectFit:"cover"}} src='./avatar.png' alt=''/>
+              <Box>
+                <Typography variant='h6' sx={{fontWeight:"700"}}>John Doe</Typography>
+                <Typography sx={{fontSize:"14px",color:"#a5a5a5"}}>Lorem ipsum dolor sit amet.</Typography>
+              </Box>
+            </Box>
+            <Box sx={{display:"flex",gap:"20px"}}>
+              <img style={{width:"20px", height:"20px"}} src='./phone.png' alt='' />
+              <img style={{width:"20px", height:"20px"}} src='./video.png' alt='' />
+              <img style={{width:"20px", height:"20px"}} src='./info.png' alt='' />
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Grid>
   )
 }
 
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import React, { useState } from 'react'
 
 const ChatList = () => {
@@ -16,6 +16,43 @@ const ChatList = () => {
               <img style={{height:"25px",width:"25px",backgroundColor:"rgba(17, 25, 40, 0.25)",padding:"10px",cursor:"pointer",borderRadius:"10px"}} src={ addMode ? './minus.png' : "./plus.png"} alt=''
               onClick={() => setAddMode(!addMode)}
               ></img>
+            </Box>
+          </Box>
+        </Box>
+        <Box>
+          <Box sx={{display:"flex",alignItems:"center",gap:"20px",padding:"20px", cursor:"pointer",borderBottom:"1px solid #dddddd35"}}>
+            <img style={{height:"50px",width:"50px",borderRadius:"50%",objectFit:"cover"}} src='./avatar.png' alt=''/>
+            <Box sx={{display:"flex",flexDirection:"column",gap:"10px"}}>
+            <Typography sx={{fontWeight:"500"}} variant='h6'>John Doe</Typography>
+            <Typography sx={{fontSize:"14px"}} variant='h'>Hello !</Typography>
+            </Box>
+          </Box>
+          <Box sx={{display:"flex",alignItems:"center",gap:"20px",padding:"20px", cursor:"pointer",borderBottom:"1px solid #dddddd35"}}>
+            <img style={{height:"50px",width:"50px",borderRadius:"50%",objectFit:"cover"}} src='./avatar.png' alt=''/>
+            <Box sx={{display:"flex",flexDirection:"column",gap:"10px"}}>
+            <Typography sx={{fontWeight:"500"}} variant='h6'>John Doe</Typography>
+            <Typography sx={{fontSize:"14px"}} variant='h'>Hello !</Typography>
+            </Box>
+          </Box>
+          <Box sx={{display:"flex",alignItems:"center",gap:"20px",padding:"20px", cursor:"pointer",borderBottom:"1px solid #dddddd35"}}>
+            <img style={{height:"50px",width:"50px",borderRadius:"50%",objectFit:"cover"}} src='./avatar.png' alt=''/>
+            <Box sx={{display:"flex",flexDirection:"column",gap:"10px"}}>
+            <Typography sx={{fontWeight:"500"}} variant='h6'>John Doe</Typography>
+            <Typography sx={{fontSize:"14px"}} variant='h'>Hello !</Typography>
+            </Box>
+          </Box>
+          <Box sx={{display:"flex",alignItems:"center",gap:"20px",padding:"20px", cursor:"pointer",borderBottom:"1px solid #dddddd35"}}>
+            <img style={{height:"50px",width:"50px",borderRadius:"50%",objectFit:"cover"}} src='./avatar.png' alt=''/>
+            <Box sx={{display:"flex",flexDirection:"column",gap:"10px"}}>
+            <Typography sx={{fontWeight:"500"}} variant='h6'>John Doe</Typography>
+            <Typography sx={{fontSize:"14px"}} variant='h'>Hello !</Typography>
+            </Box>
+          </Box>
+          <Box sx={{display:"flex",alignItems:"center",gap:"20px",padding:"20px", cursor:"pointer",borderBottom:"1px solid #dddddd35"}}>
+            <img style={{height:"50px",width:"50px",borderRadius:"50%",objectFit:"cover"}} src='./avatar.png' alt=''/>
+            <Box sx={{display:"flex",flexDirection:"column",gap:"10px"}}>
+            <Typography sx={{fontWeight:"500"}} variant='h6'>John Doe</Typography>
+            <Typography sx={{fontSize:"14px"}} variant='h'>Hello !</Typography>
             </Box>
           </Box>
         </Box>
